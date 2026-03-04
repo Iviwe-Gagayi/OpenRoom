c4261b9cf19e24442c1194ee2237a105f678310d
Iviwe Gagayi <Iviwe-Gagayi@gmail.com>
2026-03-04 09:41:15 +0200

    ...

--- app/actions/bookings.ts
+++ app/actions/bookings.ts
@@ -4,8 +4,6 @@ import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server"
 import { revalidatePath } from "next/cache";
 
-export const dynamic = 'force-dynamic';
-
 export async function createBooking(
     locationId: string,
     organizationId: string,
